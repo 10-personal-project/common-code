25d7042dc1125d11213ff5fee61370ceeeb8e565
Rayonlar <taycancille@outlook.com>
2024-10-23 21:51:16 +0800

    feat: 初始化

---
+++ src/main/java/pers/baberuth/commoncode/Constants.java
@@ -0,0 +1,91 @@
+package pers.baberuth.commoncode;
+
+import cn.hutool.core.util.ReflectUtil;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+public interface Constants
+{
+    /**
+     * 图片扩展名
+     */
+    class ImgType
+    {
+        public static final String PNG = "png";
+
+        public static final String JPG = "jpg";
+
+        public static final String JPEG = "jpeg";
+
+        public static final String HEIC = "heic";
+
+        public static final String ICO = "ico";
+
+        public static final String BMP = "bmp";
+
+        public static final String WEBP = "webp";
+
+        public static final String GIF = "gif";
+
+        public static final String TIFF = "tiff";
+
+        public static final String TIF = "tif";
+
+        public static final List<String> LIST;
+
+        static
+        {
+            Field[] fieldArr = ReflectUtil.getFields(ImgType.class, field -> field.getType().getName().equals(String.class.getTypeName()));
+            LIST = new ArrayList<>(fieldArr.length);
+            try
+            {
+                for (Field el : fieldArr)
+                {
+                    LIST.add((String) el.get(null));
+                }
+            }
+            catch (Exception e)
+            {
+                throw OptRuntimeException.getInstance(e);
+            }
+        }
+    }
+
+    class VideoType
+    {
+        public static String MOV = "mov";
+
+        public static String MP4 = "mp4";
+
+        public static String FLV = "flv";
+
+        public static String AVI = "avi";
+
+        public static String WMV = "wmv";
+
+        public static String MKV = "mkv";
+
+        public static String M4V = "m4v";
+
+        public static final List<String> LIST;
+
+        static
+        {
+            Field[] fieldArr = ReflectUtil.getFields(VideoType.class, field -> field.getType().getName().equals(String.class.getTypeName()));
+            LIST = new ArrayList<>(fieldArr.length);
+            try
+            {
+                for (Field el : fieldArr)
+                {
+                    LIST.add((String) el.get(null));
+                }
+            }
+            catch (Exception e)
+            {
+                throw OptRuntimeException.getInstance(e);
+            }
+        }
+    }
+}
---
+++ src/main/java/pers/baberuth/commoncode/Util.java
@@ -0,0 +1,62 @@
+package pers.baberuth.commoncode;
+
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.lang.Assert;
+import cn.hutool.core.util.StrUtil;
+
+import java.io.File;
+
+public class Util
+{
+    /**
+     * 判断文件是不是图片
+     * @param filePath 文件
+     * @return true-是 <br/>
+     * false-不是
+     */
+    public static boolean isImg(String filePath)
+    {
+        return isImg(FileUtil.file(filePath));
+    }
+
+    /**
+     * 判断文件是不是图片
+     * @param file 文件
+     * @return true-是 <br/>
+     * false-不是
+     */
+    public static boolean isImg(File file)
+    {
+        Assert.notNull(file, "文件不能为null");
+        Assert.isTrue(FileUtil.exist(file), "文件不存在");
+        Assert.isTrue(FileUtil.isFile(file), "不是文件");
+        String type = FileUtil.getType(file);
+        String imgTypeStr = String.join(StrUtil.COMMA, Constants.ImgType.LIST);
+        return StrUtil.containsIgnoreCase(imgTypeStr, type);
+    }
+
+    /**
+     * 判断是不是视频
+     * @param filePath 文件
+     * @return 是不是
+     */
+    public static boolean isVideo(String filePath)
+    {
+        return isVideo(FileUtil.file(filePath));
+    }
+
+    /**
+     * 判断是不是视频
+     * @param file 文件
+     * @return 是不是
+     */
+    public static boolean isVideo(File file)
+    {
+        Assert.notNull(file, "文件不能为null");
+        Assert.isTrue(FileUtil.exist(file), "文件不存在");
+        Assert.isTrue(FileUtil.isFile(file), "不是文件");
+        String type = FileUtil.getType(file);
+        String videoTypeStr = String.join(StrUtil.COMMA, Constants.VideoType.LIST);
+        return StrUtil.containsIgnoreCase(videoTypeStr, type);
+    }
+}
